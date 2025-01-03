9d50d6b1da461b11f71305c146ac716e59c3ee6b
Piyushkumar Dholariya <piyushkumar@backbase.com>
2025-01-03 10:15:01 +0530

    Removed unnecesary files

--- server.ts
+++ server.ts
@@ -2,16 +2,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import * as express from 'express';
 import { Application } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 const server_url: string = process.env.SERVER_URL || 'localhost';
 const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
-app.use(cors());
 app.disable('x-powered-by'); //Reduce fingerprinting
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

8b532cf7a7010bc320916333bb4ecfc4f8473f1d
Pallari <pallari26@gmail.com>
2025-01-09 15:35:05 +0530

    changed made in groomerValidator

--- src/validators/groomerValidator.ts
+++ src/validators/groomerValidator.ts
@@ -4,26 +4,42 @@ export const updateDetail = {
   body: Joi.object().keys({
     id: Joi.string().required(),
     groomer_showcase: Joi.object().keys({
-      groomer_cover_photo: Joi.string(),
-      groomer_gallery: Joi.array(),
+        groomer_cover_photo:Joi.string(),
+        groomer_gallery: Joi.array(),
     }),
     groomer_clinic_location: Joi.object().keys({
-      groomer_apartment_or_building: Joi.string(),
-      groomer_longitude: Joi.number(),
-      groomer_latitude: Joi.number(),
-      groomer_landmark: Joi.string(),
-      groomer_area_street_road: Joi.string()
+        groomer_apartment_or_building: Joi.string(),
+        groomer_longitude: Joi.number(),
+        groomer_latitude: Joi.number(),
+        groomer_landmark: Joi.string(),
+        groomer_area_street_road: Joi.string()
     }),
-    groomer_services: Joi.array(),
-    groomer_upload_documents: Joi.array(),
-    groomer_set_availabilty: Joi.array(),
+    groomer_services: Joi.array().items(Joi.object().keys({
+        groomer_service_image: Joi.string(),
+        groomer_service_description: Joi.string(),
+        groomer_service_size: Joi.string(),
+        groomer_service_name: Joi.string(),
+        groomer_service_price: Joi.string(),
+        groomer_service_duration: Joi.string(),
+        service_id: Joi.string()
+    })),
+    groomer_upload_documents: Joi.array().items(Joi.object().keys({
+        groomer_document_file_name: Joi.string(),
+        groomer_document_type: Joi.string(),
+        groomer_document_id: Joi.string()
+    })),
+    groomer_set_availabilty: Joi.array().items(Joi.object().keys({
+        day: Joi.string(),
+        start_time: Joi.string(),
+        end_time: Joi.string()
+    })),
     groomer_bank_info: Joi.object().keys({
-      groomer_bank_name: Joi.string(),
-      groomer_bank_account_holder: Joi.string(),
-      groomer_bank_account_number: Joi.string(),
-      reenter_groomer_bank_account_number: Joi.string(),
-      groomer_bank_ifsc_code: Joi.string(),
-      groomer_account_holder_phone: Joi.string(),
+        groomer_bank_name: Joi.string(),
+        groomer_bank_account_holder: Joi.string(),
+        groomer_bank_account_number: Joi.string(),
+        reenter_groomer_bank_account_number: Joi.string(),
+        groomer_bank_ifsc_code: Joi.string(),
+        groomer_account_holder_phone: Joi.string(),
     })
   })
 };

9cbd7a75cab37413b62d6f0602202c0c0eed58f2
Piyushkumar Dholariya <piyushkumar@backbase.com>
2025-01-10 08:12:33 +0530

    Nodemailer fixes

--- src/constants/constants-info.ts
+++ src/constants/constants-info.ts
@@ -5,12 +5,10 @@ export const SMS_API_KEY = '44d09d5a-660e-11ef-8b60-0200cd936042';
 
 
 export const MAIL_AUTH_INFO = {
+    service: 'gmail',
+    host: 'smtp.gmail.com',
     user: 'admin@doffair.com',
-    clientId: '582874779223-5r5299q39qh0vh7ndebp5008e7qf0c3j.apps.googleusercontent.com',
-    clientSecret: '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',
-    refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-    accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
-    expires: 1484314697598
+    pass: 'wcafphgeerrezobr'
 };
 
 export const RANDOM_STRING = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+';
--- src/utils/email-transport.ts
+++ src/utils/email-transport.ts
@@ -1,23 +1,15 @@
-import { apiErrorHandler, apiSuccessHandler } from '../handlers/errorHandler';
-import { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 import { MAIL_AUTH_INFO } from '../constants/constants-info';
-
 export default class EmailTransport {
 
     mailTransporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        service: MAIL_AUTH_INFO.service,
+        host: MAIL_AUTH_INFO.host,
+        port: 587,
+        secure: false,
         auth: {
-            type: 'OAuth2',
             user: MAIL_AUTH_INFO.user,
-            clientId: MAIL_AUTH_INFO.clientId,
-            clientSecret: MAIL_AUTH_INFO.clientSecret,
-            refreshToken: MAIL_AUTH_INFO.refreshToken,
-            accessToken: MAIL_AUTH_INFO.accessToken,
-            expires: MAIL_AUTH_INFO.expires
+            pass: MAIL_AUTH_INFO.pass,
         }
     });
 
@@ -25,24 +17,24 @@ export default class EmailTransport {
         this.sentVerificationEmail = this.sentVerificationEmail.bind(this);
     }
 
-    async sentVerificationEmail(req: Request, res: Response) {
+    async sentVerificationEmail(data, callback?) {
         try {
             const mailDetails = {
                 from: 'admin@doffair.com',
-                to: 'piyush.dholariya@gmail.com',
-                subject: 'Test mail',
-                text: 'Node.js testing mail for doffair'
+                to: data.to,
+                subject: data.subject,
+                text: data.text
             };
 
             this.mailTransporter.sendMail(mailDetails, (err, data) => {
                 if (err) {
-                    apiErrorHandler(err, req, res, 'Error Occurs.');
+                    callback(err, 'Error Occurs.');
                 } else {
-                    apiSuccessHandler(res, 'Email sent successfully', data);
+                    callback('Email sent successfully', data);
                 }
             });
         } catch (error) {
-            apiErrorHandler(error, req, res, 'Email sent failed.');
+            callback(error, 'Email sent failed.');
         }
     }
 }

b3655161b2453be107d28ae162d2a1abfd8d1f96
Piyushkumar Dholariya <piyushkumar@backbase.com>
2025-01-12 11:37:01 +0530

    Azure build changes

--- src/constants/constants-info.ts
+++ src/constants/constants-info.ts
@@ -3,6 +3,9 @@ export const PY_GENERATE_OTP = 'https://doffair-python-apis.azurewebsites.net/ot
 export const TWO_FACTOR_SMS_API = 'https://2factor.in/API/V1';
 export const SMS_API_KEY = '44d09d5a-660e-11ef-8b60-0200cd936042';
 
+export const DB_URL = 'mongodb+srv://doffair_dev:yTwgZQf2t3XiSXos@development-cluster.9w53x.mongodb.net/?retryWrites=true&w=majority&appName=development-cluster';
+export const DB_NAME = 'doffaire';
+
 
 export const MAIL_AUTH_INFO = {
     service: 'gmail',
